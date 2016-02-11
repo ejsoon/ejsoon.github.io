251b5a5628631f0ee246e366b945591d214a852c
ejsoonyang <e201302012123@gmail.com>
2016-02-11 18:16:13 +0800

    adaf

--- repo/count_date.js
+++ repo/count_date.js
@@ -27,12 +27,13 @@ function count_date_go(count_date, the_date) {
 	for (var ten = 5; ten > 0; ten--) {
 		point = Math.floor(sum  % (Math.pow(10, ten)) / (Math.pow(10, ten - 1)));
 		if (point >= 1) {
+			if (result.length && Math.floor(sum  % (Math.pow(10, ten + 1)) / (Math.pow(10, ten))) < 1) {
+				result += num.substr(0, 1);
+			}
 			result += num.substr(point, 1);
 			if (ten > 1) {
 				result += unit.substr(ten - 2, 1);
 			}
-		} else if (result.length && ten > 1) {
-			result += num.substr(0, 1);
 		}
 	}
 	count_date.innerHTML = result;
--- repo/repo.js
+++ repo/repo.js
@@ -1,7 +1,43 @@
+var the_navi = document.getElementsByClassName("navi_div_min");
+var the_child = document.getElementsByClassName("navi_div_child");
+var navi_num = 0;
+
+function navi_onclick(that) {
+	var the_id = that.id;
+	var the_num = the_id.substr(the_id.length - 1);
+	var add_x = 1;
+	if (the_num == 4) {
+		alert("friends");
+	} else if (navi_num == 0) {
+		while (add_x <= 3) {
+			if (add_x != the_num) {
+				the_navi[add_x - 1].style.visibility = "hidden";
+			} else {
+				navi_num = the_num;
+				the_child[navi_num * 2 - 2].style.display = "block";
+				the_child[navi_num * 2 - 1].style.display = "block";
+			}
+			add_x++;
+		}
+	} else {
+		the_child[navi_num * 2 - 2].style.display = "none";
+		the_child[navi_num * 2 - 1].style.display = "none";
+		while (add_x <= 3) {
+			if (add_x != navi_num) {
+				the_navi[add_x - 1].style.visibility = "visible";
+			}
+			add_x++;
+		}
+		navi_num = 0;
+	}
+}
+
 function the_template(route) {
 	var clear_float_div = document.createElement("div");
 	var head_div = document.getElementById("head_div");
 	var navi_div = document.getElementById("navi_div");
+	var content_div = document.getElementById("content_div");
+	var body01 = document.getElementsByTagName("body");
 	var bottom_div = document.getElementById("bottom_div");
 
 	var p = 0;
@@ -37,17 +73,20 @@ function the_template(route) {
 	head_div.appendChild(head_title_out);
 
 	//navi_div
-//<div id="navi_div_min1" class="navi_div_min" onclick="navi_onclick(this)"><img 
 	var navi_div_min = [];
 	var navi_div_min_bg = [];
 	var navi_div_min_inner = [];
-	var navi_div_min_innerHTML = ["文", "言", "忠", "信"];
+	var navi_div_min_innerHTML = ["文", "行", "忠", "信"];
 	var navi_link = [];
+	var navi_link_href = ["/blog", "/cc", "/world", "/live", "/online", "/learn"];
 	var navi_div_child = [];
 	var navi_div_child_bg = [];
+	var navi_div_child_inner = [];
+	var navi_div_child_innerHTML = ["言", "字", "棊", "道", "用", "學"];
 
 	for (p = 0; p < 4; p++) {
 		navi_div_min.push(document.createElement("div"));
+		navi_div_min[p].id = "navi_div_min" + String(p + 1);
 		navi_div_min[p].className = "navi_div_min";
 		navi_div_min[p].onclick = function(){navi_onclick(this)};
 	
@@ -64,8 +103,29 @@ function the_template(route) {
 	
 		navi_div.appendChild(navi_div_min[p]);
 	}
+	for (p = 0; p < 6; p++) {
+		navi_link.push(document.createElement("a"));
+		navi_link[p].className = "navi_link";
+		navi_link[p].href = route + navi_link_href[p] + "/index.html";
+		navi_link[p].target = "__blank";
 
- //class="navi_link" href="blog/index.html" target="__blank"
+		navi_div_child.push(document.createElement("div"));
+		navi_div_child[p].id = "navi_div_child" + String(p + 1);
+		navi_div_child[p].className = "navi_div_child";
+
+		navi_div_child_bg.push(document.createElement("img"));
+		navi_div_child_bg[p].className = "navi_div_min_bg";
+		navi_div_child_bg[p].src = route + "/pictures/star.png";
+
+		navi_div_child_inner.push(document.createElement("div"));
+		navi_div_child_inner[p].className = "navi_div_min_inner";
+		navi_div_child_inner[p].innerHTML = navi_div_child_innerHTML[p];
+
+		navi_div_child[p].appendChild(navi_div_child_bg[p]);
+		navi_div_child[p].appendChild(navi_div_child_inner[p]);
+		navi_link[p].appendChild(navi_div_child[p]);
+		navi_div.appendChild(navi_link[p]);
+	}
 
 	//bottom_icon
 	var bottom_icon = document.createElement("img");
@@ -76,18 +136,8 @@ function the_template(route) {
 	bottom_link.target = "__blank";
 	bottom_link.appendChild(bottom_icon);
 	bottom_div.appendChild(bottom_link);
-/*
-                    newSpan[dataPoint] = document.createElement("span");
-                    dataPath[dataPoint] = dataSplit[dataPoint].split(":")[0];
-                    newText[dataPoint] = dataSplit[dataPoint].split(":")[1];
-                    dataTitle[dataPoint] = document.createTextNode(newText[dataPoint]);
-                    newSpan[dataPoint].appendChild(document.createTextNode(" "));
-                    newTag_a[dataPoint].href = dataPath[dataPoint];
-                    newTag_a[dataPoint].onclick = function(){change_html(this.hash)};
-                    newTag_a[dataPoint].className = "a_index";
-                    newTag_a[dataPoint].appendChild(dataTitle[dataPoint]);
 
-*/
-
+	//content bg
+	body01[0].style.backgroundImage = "url(" + route + "/pictures/index/content_bg.png)";
 }
 
